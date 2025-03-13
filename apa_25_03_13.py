6c5fe28366552771f7958b756a3d46aee6245ad1
Biel Bernal Pratdesaba <biel.bernal@estudiantat.upc.edu>
2025-03-13 18:52:02 +0100

    Primera funció

---
+++ apa_25_03_13.py
@@ -0,0 +1,4 @@
+# coding: utf-8
+get_ipython().run_line_magic('run', 'primos')
+get_ipython().run_line_magic('run', 'primos -v')
+help(esPrimo)
--- primos.py
+++ primos.py
@@ -1,10 +1,31 @@
 """
 Biel Bernal Pratdesaba
+
+>>> esPrimo(5)
+True
+
+>>> esPrimo(2)
+True
+
+>>> esPrimo(74211)
+False
+
 """ 
 
 def esPrimo(numero,):
     """
     Devuelve true si el numero es primo y false si no lo es.
+
+    >>> for numero in range(2, 10): 
+    ...     print(esPrimo(numero))
+    True
+    True
+    False
+    True
+    False
+    True
+    False
+    False
     """
     for prueva in range(2, numero):
         if numero % prueva == 0:
@@ -12,5 +33,6 @@ def esPrimo(numero,):
     return True
 
 
-
-    
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
